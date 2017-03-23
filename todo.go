2002a44a832413c9f0d11e0d8f64de036da3b4c1
Vasiliy Shubko <flit77@gmail.com>
2017-03-23 22:41:36 +0100

    database init

--- todo.go
+++ todo.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"database/sql"
+
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	// Create a new instance of Echo
+	db := initDB("storage.db")
+	migrate(db)
+
 	e := echo.New()
 
 	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
 	e.PUT("/tasks", func(c echo.Context) error { return c.JSON(200, "PUT Tasks") })
 	e.DELETE("/tasks/:id", func(c echo.Context) error { return c.JSON(200, "DELETE Task "+c.Param("id")) })
 
-	// Start as a web server
-	e.Run(standard.New(":8000"))
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nil")
+	}
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `
+    CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL
+    );
+    `
+
+	_, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
 }
